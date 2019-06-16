a1c5718032bccdfce425d0fb3b88291070c7eaef
rudravaram85 <rajesh.rudravaram85@gmail.com>
2019-06-16 14:56:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-iv3m8t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-iv3m8t)
---
+++ index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import axios from 'axios';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const name = res.data;
+        console.log(name.name);
+        this.setState({ name });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
